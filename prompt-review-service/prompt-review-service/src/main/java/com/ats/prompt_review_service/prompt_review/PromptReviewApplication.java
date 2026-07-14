efae1c5eadb827fd843d537ec6a7229b1a09b6cd
Abuzar Mehdi <abuzarmehdi214@gmail.com>
2026-07-14 10:26:43 +0500

    initialized prompt-review-service

---
+++ prompt-review-service/prompt-review-service/src/main/java/com/ats/prompt_review_service/prompt_review/PromptReviewApplication.java
@@ -0,0 +1,13 @@
+package com.ats.prompt_review_service.prompt_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptReviewApplication.class, args);
+	}
+
+}
